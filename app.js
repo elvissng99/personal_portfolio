2661c9f5c61da2c687756f6d0be8edcc14959be3
elvissng99 <elvissng99@gmail.com>
2022-09-29 07:45:18 +0800

    complete code change
    added file upload

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ const express = require("express")
 const expressHandlebars = require('express-handlebars')
 const sqlite3 = require("sqlite3")
 const expressSession = require('express-session')
+const multer = require("multer")
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination: "./public/",
+    filename: function(request,file,callback){
+        if (file!=undefined){
+            const currentDate = Date.now()
+            request.body.imagePath = "/" + currentDate + path.extname(file.originalname)
+            callback(null,currentDate + path.extname(file.originalname))
+        }
+    }
+})
+
+const upload = multer({
+    storage:storage,    
+}).single('imagePath')
 
 const ADMIN_USERNAME ="kaitao"
 const ADMIN_PASSWORD = "kaitao123"
 
-const PROJECT_TITLE_MAX_LENGTH = 50;
+const TITLE_MAX_LENGTH = 50;
+const ABOUT_DESCRIPTION_MAX_LENGTH = 2000;
 const PROJECT_DESCRIPTION_MAX_LENGTH = 500
 const IMAGE_PATH_MAX_LENGTH = 100
 
@@ -23,6 +41,7 @@ db.run(`
 db.run(`
     CREATE TABLE IF NOT EXISTS about (
 		id INTEGER PRIMARY KEY,
+        title TEXT,
 		description TEXT
 	);
 `)
@@ -47,6 +66,7 @@ app.use(express.static("public"))
 app.use(express.urlencoded({
     extended:false
 }))
+app.use(express.json());
 
 // app.use(express.json())
 // app.use(expressSession({
@@ -88,18 +108,62 @@ app.get('/', function(request,response){
 
 app.get('/about', function(request,response){
     const query = `SELECT * FROM about`
-    db.all(query,function(error,about){
+    db.get(query,function(error,about){
         const errorMessages = []
         if(error){
             errorMessages.push("Internal server error")
         }
-
         const model = {
             errorMessages,
-            about
+            about,
+            session:request.session,
+
         }
         response.render('about.hbs',model)
     })
+})
+
+app.post('/about',function(request,response){
+    const title = request.body.title
+    const description = request.body.description
+    let successful = false
+    const errorMessages = []
+
+    if (title == ""){
+        errorMessages.push("Title can't be empty")
+    }else if (TITLE_MAX_LENGTH < title.length){
+        errorMessages.push("Title may be at most " +TITLE_MAX_LENGTH + " characters long")
+    }
+
+    if (description ==""){
+        errorMessages.push("Description should not be empty")
+    }else if (ABOUT_DESCRIPTION_MAX_LENGTH < description.length){
+        errorMessages.push("Description may be at most " +ABOUT_DESCRIPTION_MAX_LENGTH + " characters long")
+    }
+
+    if(!request.session.isLoggedIn){
+        errorMessages.push("Not logged in")
+    }
+
+    if(errorMessages.length == 0){
+        const query = `UPDATE about SET title = ?, description = ? WHERE id = 1`
+        const values = [title,description]
+        db.run(query,values,function(error){
+            if(error){
+                errorMessages.push("Internal server error")
+            }
+        })
+    }
+    if(errorMessages.length == 0) successful = true
+    const model = {
+        errorMessages,
+        successful,
+        session:request.session,
+        about:{
+            title:title,
+            description:description}
+    }
+    response.render('about.hbs', model)
 })
 
 app.get('/addProject', function(request,response){
@@ -110,18 +174,16 @@ app.get('/addProject', function(request,response){
     }
 })
 
-app.post('/addProject', function(request,response){
+app.post('/addProject', upload, function(request,response){
     const title = request.body.title
     const description = request.body.description
-    //currently it is fixed for now
-    const imagePath = "/image1.png"
+    const imagePath = request.body.imagePath
 
     const errorMessages = []
-
     if (title == ""){
         errorMessages.push("Title can't be empty")
-    }else if (PROJECT_TITLE_MAX_LENGTH < title.length){
-        errorMessages.push("Title may be at most " +PROJECT_TITLE_MAX_LENGTH + " characters long")
+    }else if (TITLE_MAX_LENGTH < title.length){
+        errorMessages.push("Title may be at most " +TITLE_MAX_LENGTH + " characters long")
     }
 
     if (description ==""){
@@ -130,40 +192,43 @@ app.post('/addProject', function(request,response){
         errorMessages.push("Description may be at most " +PROJECT_DESCRIPTION_MAX_LENGTH + " characters long")
     }
 
-    //do image path checks when figured out
-
     if(!request.session.isLoggedIn){
         errorMessages.push("Not logged in")
     }
 
+    if(imagePath == undefined){
+        errorMessages.push("Please attach a file")
+    }
+
     if(errorMessages.length == 0){
-        const query = `INSERT INTO projects (title,description,imagePath) VALUES (?,?)`
+        const query = `INSERT INTO projects (title,description,imagePath) VALUES (?,?,?)`
         const values = [title,description,imagePath]
         db.run(query,values,function(error){
             if(error){
                 errorMessages.push("Internal server error")
                 const model = {
                     errorMessages,
-                    title,
-                    description,
-                    //also pass in image stuff if possible
+                    project:{
+                        title:title,
+                        description:description,
+                        imagePath:imagePath                    }
                 }
-                response.render('addProject.hbs')
+                response.render('addProject.hbs', model)
             }else{
-                response.redirect("/admin")
+                response.redirect("/")
             }
         })
     }else{
         const model = {
             errorMessages,
-            title,
-            description,
-            //also passi n image stuff if possible
+            project:{
+                title:title,
+                description:description,
+                imagePath:imagePath            }
+            
         }
         response.render("addProject.hbs",model)
     }
-
-    response.render("addProject.hbs")
 })
 
 app.get('/project/:id',function(request,response){
@@ -174,9 +239,111 @@ app.get('/project/:id',function(request,response){
     db.get(query, values, function(error,project){
         const model = {
             project :project,
-            isLoggedIn: request.session.isLoggedIn
+            session:request.session,
         }
         response.render('project.hbs', model)
+    })
+})
+
+app.get('/editProject/:id',function(request,response){
+    const id = request.params.id
+    const query = `SELECT * from projects where id =?`
+    const values = [id]
+    db.get(query, values, function(error,project){
+        const model = {
+            id,
+            project :project,
+            session:request.session,
+        }
+        response.render('editProject.hbs', model)
+    })
+})
+
+app.post('/editProject/:id',upload,function(request,response){
+    const title = request.body.title
+    const description = request.body.description
+    const imagePath = request.body.imagePath
+
+    const errorMessages = []
+    if (title == ""){
+        errorMessages.push("Title can't be empty")
+    }else if (TITLE_MAX_LENGTH < title.length){
+        errorMessages.push("Title may be at most " +TITLE_MAX_LENGTH + " characters long")
+    }
+
+    if (description ==""){
+        errorMessages.push("Description should not be empty")
+    }else if (PROJECT_DESCRIPTION_MAX_LENGTH < description.length){
+        errorMessages.push("Description may be at most " +PROJECT_DESCRIPTION_MAX_LENGTH + " characters long")
+    }
+
+    if(!request.session.isLoggedIn){
+        errorMessages.push("Not logged in")
+    }
+
+    if(errorMessages.length == 0){
+        let query
+        let values
+        if (imagePath== undefined){
+            query = `UPDATE projects SET title = ?, description = ? WHERE id = ?`
+            values = [title,description, request.params.id]
+        }else{
+            query = `UPDATE projects SET title = ?, description = ?, imagePath = ? WHERE id = ?`
+            values = [title,description,imagePath, request.params.id]
+        }
+        
+        db.run(query,values,function(error){
+            if(error){
+                errorMessages.push("Internal server error")
+                const model = {
+                    errorMessages,
+                    project:{
+                        title:title,
+                        description:description,
+                        imagePath:imagePath
+                    }
+                }
+                response.render('editProject.hbs', model)
+            }else{
+                response.redirect("/")
+            }
+        })
+    }else{
+        const model = {
+            errorMessages,
+            project:{
+                title:title,
+                description:description,
+                imagePath:imagePath
+            }
+            
+        }
+        response.render("editProject.hbs",model)
+    }
+})
+
+app.get('/deleteProject/:id',function(request,response){
+    const id = request.params.id
+    response.render("deleteProject.hbs",{id})
+})
+
+app.post('/deleteProject/:id', function(request,response){
+    const id = request.params.id
+    const query = `DELETE FROM projects WHERE id =?`
+    const values = [id]
+    db.run(query, values,function(error){
+        if (error){
+            const errorMessages = []
+            errorMessages.push("Internal Server Error")
+            response.render("deleteProject.hbs",{errorMessages})
+        }else{
+            const model = {
+                id,
+                session:request.session,
+            }
+            response.redirect('/')
+        }
+        
     })
 })
 
@@ -189,7 +356,7 @@ app.post('/login', function(request,response){
     const password = request.body.password
     if(username == ADMIN_USERNAME && password == ADMIN_PASSWORD){
         request.session.isLoggedIn = true
-        response.redirect('/admin')
+        response.redirect('/')
     }else{
         const model = {
             failedToLogin: true
@@ -199,42 +366,13 @@ app.post('/login', function(request,response){
 })
 
 app.get('/logout',function(request,response){
-    request.session.destroy(function(err){
-        if (err){
-            throw err
+    request.session.destroy(function(error){
+        if (error){
+            response.redirect("/")
         }else{
             response.redirect("/login")
         }
     })
-})
-
-app.get('/admin', function(request,response){
-    if (request.session.isLoggedIn){
-        const query = `SELECT * FROM home`
-        db.get(query,function(error,home){
-            const errorMessages = []
-            if(error){
-                errorMessages.push("Internal server error")
-            }
-            const query2 = `SELECT * FROM projects`
-            db.all(query2,function(error,projects){
-                const errorMessages = []
-                if(error){
-                    errorMessages.push("Internal server error")
-                }
-                
-                const model = {
-                    session:request.session,
-                    errorMessages,
-                    home,
-                    projects
-                }
-                response.render('admin.hbs',model)
-            })
-        })
-    }else{
-        response.redirect('/login')
-    }
 })
 
 app.get('/resume',function (request,response){
