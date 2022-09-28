00b43c3d47d950b0802d4de35dedd31955b700c9
elvissng99 <elvissng99@gmail.com>
2022-09-29 07:51:47 +0800

    removing excess files

--- server_elvis/routes/navigation.js
+++
@@ -1,8 +0,0 @@
-const express= require('express')
-const router = express.Router()
-const navigationController = require("../controllers/navigationController")
-
-router.get('/', navigationController.home)
-router.get('/resume', navigationController.resume)
-
-module.exports = router
