4d4e9b33d1985ac9dd6c01ff80a0847d91244680
elvissng99 <elvissng99@gmail.com>
2022-10-08 03:10:14 +0800

    deleted main

--- password.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-const myPlaintextPassword = 'ComputerScience123';
-
-bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-    console.log(hash)
-});
